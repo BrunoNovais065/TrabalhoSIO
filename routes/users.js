cff082d029949951cdf3f62c6b740d850d2f4117
BrunoNovais065 <116166827+BrunoNovais065@users.noreply.github.com>
2024-06-03 20:09:11 +0100

    opção de outros anos adicionado

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,23 @@ var GraphController = require('../controllers/getGraphsController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' ,year: '2023'});
+});
+
+router.get('/otheryear2023', function(req, res, next) {
+  res.render('index', { title: 'Express' ,year: '2023'});
+});
+router.get('/otheryear2024', function(req, res, next) {
+  res.render('yearNotWork', { title: 'Express' ,year: '2024'});
+});
+router.get('/otheryear2022', function(req, res, next) {
+  res.render('yearNotWork', { title: 'Express' ,year: '2022'});
+});
+router.get('/otheryear2021', function(req, res, next) {
+  res.render('yearNotWork', { title: 'Express' ,year: '2021'});
+});
+router.get('/otheryear2020', function(req, res, next) {
+  res.render('yearNotWork', { title: 'Express' ,year: '2020'});
 });
 
 
@@ -20,5 +36,6 @@ router.post('/directory4', GraphController.getGraphsdiagram4);
 router.post('/directory5', GraphController.getGraphsdiagram5);
 
 router.post('/directory6', GraphController.getGraphsdiagram6);
+
 
 module.exports = router;
