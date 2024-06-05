2e087d129e8b310aea6391f95b5dc975557e4630
BrunoNovais065 <116166827+BrunoNovais065@users.noreply.github.com>
2024-06-05 22:22:46 +0100

    alguns graficos novo

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,50 @@ router.get('/otheryear2020', function(req, res, next) {
   res.render('yearNotWork', { title: 'Express' ,year: '2020'});
 });
 
+//router para paginas da diretory 4
+
+
+//router para paginas da diretory 5
+router.get('/directory5/Electroteck', GraphController.getGraphsdiagram5Electroteck);
+router.get('/directory5/Portugal/Electrotech', GraphController.getGraphsdiagram5PortugalElectrotech);
+router.get('/directory5/Spain/Electrotech', GraphController.getGraphsdiagram5SpainElectrotech);
+router.get('/directory5/Q1/Electrotech', GraphController.getGraphsdiagram5Q1Electrotech);
+router.get('/directory5/Q2/Electrotech', GraphController.getGraphsdiagram5Q2Electrotech);
+router.get('/directory5/Q3/Electrotech', GraphController.getGraphsdiagram5Q3Electrotech);
+router.get('/directory5/Q4/Electrotech', GraphController.getGraphsdiagram5Q4Electrotech);
+router.get('/directory5/January/Electrotech', GraphController.getGraphsdiagram5JanuaryElectrotech);
+router.get('/directory5/February/Electrotech', GraphController.getGraphsdiagram5FebruaryElectrotech);
+router.get('/directory5/March/Electrotech', GraphController.getGraphsdiagram5MarchElectrotech);
+router.get('/directory5/April/Electrotech', GraphController.getGraphsdiagram5AprilElectrotech);
+router.get('/directory5/May/Electrotech', GraphController.getGraphsdiagram5MayElectrotech);
+router.get('/directory5/June/Electrotech', GraphController.getGraphsdiagram5JuneElectrotech);
+router.get('/directory5/July/Electrotech', GraphController.getGraphsdiagram5JulyElectrotech);
+router.get('/directory5/August/Electrotech', GraphController.getGraphsdiagram5AugustElectrotech);
+router.get('/directory5/September/Electrotech', GraphController.getGraphsdiagram5SeptemberElectrotech);
+router.get('/directory5/October/Electrotech', GraphController.getGraphsdiagram5OctoberElectrotech);
+router.get('/directory5/November/Electrotech', GraphController.getGraphsdiagram5NovemberElectrotech);
+router.get('/directory5/December/Electrotech', GraphController.getGraphsdiagram5DecemberElectrotech);
+
+router.get('/directory5/KrackenTech', GraphController.getGraphsdiagram5KrackenTech);
+router.get('/directory5/Portugal/KrackenTech', GraphController.getGraphsdiagram5PortugalKrackenTech);
+router.get('/directory5/Q1/KrackenTech', GraphController.getGraphsdiagram5Q1KrackenTech);
+router.get('/directory5/Q2/KrackenTech', GraphController.getGraphsdiagram5Q2KrackenTech);
+router.get('/directory5/Q3/KrackenTech', GraphController.getGraphsdiagram5Q3KrackenTech);
+router.get('/directory5/Q4/KrackenTech', GraphController.getGraphsdiagram5Q4KrackenTech);
+router.get('/directory5/January/KrackenTech', GraphController.getGraphsdiagram5JanuaryKrackenTech);
+router.get('/directory5/February/KrackenTech', GraphController.getGraphsdiagram5FebruaryKrackenTech);
+router.get('/directory5/March/KrackenTech', GraphController.getGraphsdiagram5MarchKrackenTech);
+router.get('/directory5/April/KrackenTech', GraphController.getGraphsdiagram5AprilKrackenTech);
+router.get('/directory5/May/KrackenTech', GraphController.getGraphsdiagram5MayKrackenTech);
+router.get('/directory5/June/KrackenTech', GraphController.getGraphsdiagram5JuneKrackenTech);
+router.get('/directory5/July/KrackenTech', GraphController.getGraphsdiagram5JulyKrackenTech);
+router.get('/directory5/August/KrackenTech', GraphController.getGraphsdiagram5AugustKrackenTech);
+router.get('/directory5/September/KrackenTech', GraphController.getGraphsdiagram5SeptemberKrackenTech);
+router.get('/directory5/October/KrackenTech', GraphController.getGraphsdiagram5OctoberKrackenTech);
+router.get('/directory5/November/KrackenTech', GraphController.getGraphsdiagram5NovemberKrackenTech);
+router.get('/directory5/December/KrackenTech', GraphController.getGraphsdiagram5DecemberKrackenTech);
+
+
 
 router.post('/directory1', GraphController.getGraphsdiagram1);
 
