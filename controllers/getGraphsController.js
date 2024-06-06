eec99f2a4ce111e8b58e5b761dbf6086707596f9
Fábio Sousa <8190181@estg.ipp.pt>
2024-06-07 00:14:27 +0100

    c
    c

--- controllers/getGraphsController.js
+++ controllers/getGraphsController.js
@@ -2,46 +2,28 @@ var GraphController = {};
 
 const sql = require("msnodesqlv8");
 
-/*const con = sql.createConnection({
-    host: "192.168.1.106",
-    user: "Bruno",
-    password: "bruno200602",
-    database: "Bookings"
-});
+GraphController.getGraphsdiagram1 = function (req, res) {
+	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("SELECT * FROM dbo.Guest", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
-});*/
-
-GraphController.getGraphsdiagram5 = function (req, res) {
-	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};`;
-
+	
 	let query = '';
-	if (req.body.country == 'null' && req.body.trim == 'null') {
-		query = 'SELECT f.s_key, f.net_total FROM facts AS f, dim_places AS p WHERE f.s_key = p.s_key';
+	if (req.body.product_id == "null" && req.body.trim == "null") {
+		query = `SELECT f.s_key, f.net_total FROM facts AS f WHERE f.s_key LIKE 'TECH%' AND f.s_key LIKE '%SUPPLIER'`;
 	} else {
-		query = 'SELECT * FROM facts AS f WHERE ';
+		query = "SELECT * FROM facts AS f WHERE ";
 
 		let t_query = '';
-		if (req.body.trim != 'null') {
+		if (req.body.trim != "null") {
 			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
-			if (req.body.month != 'null') { t_query = t_query.concat(";" + req.body.month + "|"); }
-			query = query.concat(t_query + '|%\' ');
+			if (req.body.month != "null") { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + "|%' ");
 		}
 
-		let p_query = ''; if (req.body.trim != 'null') { p_query = ' AND '; }
-		if (req.body.country != 'null') {
-			p_query = p_query.concat(`f.s_key LIKE '%|${req.body.country}`);
-			if (req.body.city != 'null') { p_query = p_query.concat(`;${req.body.city}`); }
-			query = query.concat(p_query + '\'');
-		} else {
-			query = query.concat(`AND (f.s_key LIKE '%|Portugal' OR f.s_key LIKE '%|Espanha')`);
-		}
+		let p_query = ''; if (req.body.trim != "null") { p_query = "AND "; }
+		if (req.body.product_id != "null") {
+			query = query + p_query.concat(`f.s_key LIKE '%|${req.body.product_id};%' `);
+		} else { query = query.concat('AND f.s_key LIKE \'%|TECH%\''); }
+		query = query.concat(`AND f.s_key LIKE '%SUPPLIER'`);
 	}
 
 	sql.query(connectionString, query, (err, result) => {
@@ -53,87 +35,36 @@ GraphController.getGraphsdiagram5 = function (req, res) {
 			name: row.s_key,
 			value: row.net_total,
 		}));
-			console.log(Data);
-		res.render("newgraph5", { graphData: Data });
+
+		for (i = 0; i < Data.length; i++) {
+			let match = Data[i].name.match(/\d+/);
+			Data[i].name = Data[i].name.split(";")[0];
+		}
+
+		res.render("graph1", { graphData: Data });
 	});
 };
 
-GraphController.getGraphsdiagram1 = function (req, res) {
-	console.log(req.body);
-	if (req.body.company == "KrakenTech") {
-		console.log("KrakenTech");
-
-		const connectionString =
-			"server=.;Database=KrakenTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `
-        SELECT *
-FROM dbo.facts
-WHERE s_key LIKE '%SUPPLIER'
-ORDER BY CAST(SUBSTRING(s_key, 5, CHARINDEX(';', s_key) - 5) AS INT);
-`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			for (i = 0; i < Data.length; i++) {
-				let match = Data[i].name.match(/\d+/);
-				//the name has the constructions of TECH1;SUPPLIER short it by number
-				Data[i].name = Data[i].name.split(";")[0];
-			}
-
-			res.render("graph1", { graphData: Data });
-		});
-	} else {
-		console.log("eletrotech");
-
-		const connectionString =
-			"server=.;Database=ElectroTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `
-        SELECT *
-FROM dbo.facts
-WHERE s_key LIKE '%SUPPLIER'
-ORDER BY CAST(SUBSTRING(s_key, 5, CHARINDEX(';', s_key) - 5) AS INT);
-`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			for (i = 0; i < Data.length; i++) {
-				let match = Data[i].name.match(/\d+/);
-				//the name has the constructions of TECH1;SUPPLIER short it by number
-				Data[i].name = Data[i].name.split(";")[0];
-			}
-
-			res.render("graph1", { graphData: Data });
-		});
-	}
-};
-
 GraphController.getGraphsdiagram2 = function (req, res) {
-	const connectionString =
-		`server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};`;
+	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
 
-	const query = `SELECT * FROM dbo.facts WHERE s_key LIKE '%Fornecedor%' `;
-	if (req.body.trimester != 'null') {
-		query = query.concat(`AND f.s_key LIKE '2023;${req.body.trimester}`);
-		if (req.body.month != 'null') { query = query.concat(";" + req.body.month); }
-		query = query.concat("|%'");
+	let query = '';
+	if (req.body.supplier == "null" && req.body.trim == "null") {
+		query = `SELECT f.s_key, f.net_total FROM facts AS f, dim_suppliers AS s WHERE f.s_key = s.s_key`;
+	} else {
+		query = "SELECT * FROM facts AS f WHERE ";
+
+		let t_query = '';
+		if (req.body.trim != "null") {
+			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
+			if (req.body.month != "null") { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + "|%' ");
+		}
+
+		let p_query = ''; if (req.body.trim != "null") { p_query = "AND "; }
+		if (req.body.supplier != "null") {
+			query = query + p_query.concat(`f.s_key LIKE '%;${req.body.supplier};%' `);
+		} else { query = query.concat(`AND f.s_key LIKE '%;Fornecedor%'`); }
 	}
 
 	sql.query(connectionString, query, (err, result) => {
@@ -160,11 +91,28 @@ GraphController.getGraphsdiagram2 = function (req, res) {
 };
 
 GraphController.getGraphsdiagram3 = function (req, res) {
-	const connectionString =
-		`server=.;Database=${req.body.company};Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};`;
+	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
+	
+	let query = '';
+	if (req.body.client == "null" && req.body.trim == "null") {
+		query = `SELECT * FROM facts AS f, dim_clients AS c WHERE f.s_key = c.s_key`;
+	} else {
+		query = "SELECT * FROM facts AS f WHERE ";
 
-	const queryy = `SELECT * FROM facts AS f WHERE f.s_key LIKE '%CLIENT%'`;
-	sql.query(connectionString, queryy, (err, result) => {
+		let t_query = '';
+		if (req.body.trim != "null") {
+			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
+			if (req.body.month != "null") { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + "|%' ");
+		}
+
+		let p_query = ''; if (req.body.trim != "null") { p_query = "AND "; }
+		if (req.body.client != "null") {
+			query = query + p_query.concat(`f.s_key LIKE '%${req.body.client};%'`);
+		} else { query = query.concat(`AND f.s_key NOT LIKE '%Fornecedor%'`); }
+	}
+
+	sql.query(connectionString, query, (err, result) => {
 		if (err) {
 			console.log(err);
 			return;
@@ -188,225 +136,114 @@ GraphController.getGraphsdiagram3 = function (req, res) {
 };
 
 GraphController.getGraphsdiagram4 = function (req, res) {
-	if (req.body.company == "KrakenTech") {
-		console.log("KrakenTech");
+	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
 
-		const connectionString =
-			"server=.;Database=KrakenTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `SELECT * FROM dbo.facts WHERE s_key LIKE '%SUPPLIER'
-				ORDER BY CAST(SUBSTRING(s_key, 5, CHARINDEX(';', s_key) - 5) AS INT);`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			for (i = 0; i < Data.length; i++) {
-				let match = Data[i].name.match(/\d+/);
-				//the name has the constructions of TECH1;SUPPLIER short it by number
-				Data[i].name = Data[i].name.split(";")[0];
-			}
-
-			res.render("graph4", { graphData: Data });
-		});
+	let query = '';
+	if (req.body.product_id == "null" && req.body.trim == "null") {
+		query = `SELECT f.s_key, f.net_total FROM facts AS f WHERE f.s_key LIKE 'TECH%' AND f.s_key LIKE '%CLIENT'`;
 	} else {
-		console.log("eletrotech");
+		query = "SELECT * FROM facts AS f WHERE ";
 
-		const connectionString =
-			"server=.;Database=ElectroTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
+		let t_query = '';
+		if (req.body.trim != "null") {
+			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
+			if (req.body.month != "null") { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + "|%' ");
+		}
 
-		const queryy = `
-        SELECT *
-FROM dbo.facts
-WHERE s_key LIKE '%SUPPLIER'
-ORDER BY CAST(SUBSTRING(s_key, 5, CHARINDEX(';', s_key) - 5) AS INT);
-`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			for (i = 0; i < Data.length; i++) {
-				let match = Data[i].name.match(/\d+/);
-				//the name has the constructions of TECH1;SUPPLIER short it by number
-				Data[i].name = Data[i].name.split(";")[0];
-			}
-
-			res.render("graph4", { graphData: Data });
-		});
+		let p_query = ''; if (req.body.trim != "null") { p_query = "AND "; }
+		if (req.body.product_id != "null") {
+			query = query + p_query.concat(`f.s_key LIKE '%|${req.body.product_id};%' `);
+		} else { query = query.concat('AND f.s_key LIKE \'%|TECH%\''); }
+		query = query.concat(`AND f.s_key LIKE '%CLIENT'`);
 	}
+
+	sql.query(connectionString, query, (err, result) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		const Data = result.map((row) => ({
+			name: row.s_key,
+			value: row.net_total,
+		}));
+
+		for (i = 0; i < Data.length; i++) {
+			let match = Data[i].name.match(/\d+/);
+			//the name has the constructions of TECH1;SUPPLIER short it by number
+			Data[i].name = Data[i].name.split(";")[0];
+		}
+
+		res.render("graph4", { graphData: Data });
+	});
 };
 
-/**
 GraphController.getGraphsdiagram5 = function (req, res) {
-	if (req.body.company == "KrakenTech") {
-		console.log("KrakenTech");
-
-		const connectionString =
-			"server=.;Database=KrakenTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `
-        SELECT *
-FROM dbo.facts
-`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = [];
-
-			Data.push({
-				name: result[32].s_key,
-				value: result[32].net_total,
-			});
-			Data.push({
-				name: result[33].s_key,
-				value: result[33].net_total,
-			});
-			Data.push({
-				name: result[34].s_key,
-				value: result[34].net_total,
-			});
-			Data.push({
-				name: result[35].s_key,
-				value: result[35].net_total,
-			});
-			Data.push({
-				name: result[36].s_key,
-				value: result[36].net_total,
-			});
-			Data.push({
-				name: result[37].s_key,
-				value: result[37].net_total,
-			});
-
-			res.render("newgraph5", { graphData: Data });
-		});
-	} else {
-		console.log("eletrotech");
-
-		const connectionString =
-			"server=.;Database=ElectroTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `SELECT * FROM dbo.facts`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = [];
-
-			Data.push({
-				name: result[38].s_key,
-				value: result[38].net_total,
-			});
-			Data.push({
-				name: result[39].s_key,
-				value: result[39].net_total,
-			});
-			Data.push({
-				name: result[40].s_key,
-				value: result[40].net_total,
-			});
-			Data.push({
-				name: result[41].s_key,
-				value: result[41].net_total,
-			});
-			Data.push({
-				name: result[42].s_key,
-				value: result[42].net_total,
-			});
-			Data.push({
-				name: result[43].s_key,
-				value: result[43].net_total,
-			});
-			Data.push({
-				name: result[44].s_key,
-				value: result[44].net_total,
-			});
-			Data.push({
-				name: result[45].s_key,
-				value: result[45].net_total,
-			});
-			Data.push({
-				name: result[46].s_key,
-				value: result[46].net_total,
-			});
-			Data.push({
-				name: result[47].s_key,
-				value: result[47].net_total,
-			});
-			res.render("graph5Electro", { graphData: Data });
-		});
-	}
-};
-*/
-
-GraphController.getGraphsdiagram6 = function (req, res) {
-	if (req.body.company == "KrakenTech") {
-		console.log("KrakenTech");
-
-		const connectionString =
-			"server=.;Database=KrakenTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `SELECT top 17 * FROM dbo.facts`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			res.render("newgraph6", { graphData: Data });
-		});
-	} else {
-		console.log("eletrotech");
-		const connectionString =
-			"server=.;Database=ElectroTech_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
-
-		const queryy = `SELECT top 17 * FROM dbo.facts`;
-
-		sql.query(connectionString, queryy, (err, result) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const Data = result.map((row) => ({
-				name: row.s_key,
-				value: row.net_total,
-			}));
-
-			res.render("newgraph6", { graphData: Data });
-		});
-	}
-};
-
-GraphController.getGraphsdiagram1Time = function(req, res) {
-
 	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};`;
 
-	let query = "SELECT * FROM facts AS f WHERE f.s_key LIKE '%SUPPLIER' AND f.s_key LIKE '2023;" + req.body.trimester;
-	if (req.body.month != 'null') { query = query.concat(req.body.month); }
-	query = query.concat("|%");
+	let query = '';
+	if (req.body.country == 'null' && req.body.trim == 'null') {
+		query = 'SELECT f.s_key, f.net_total FROM facts AS f, dim_places AS p WHERE f.s_key = p.s_key';
+	} else {
+		query = 'SELECT * FROM facts AS f WHERE ';
+
+		let t_query = '';
+		if (req.body.trim != 'null') {
+			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
+			if (req.body.month != 'null') { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + '|%\' ');
+		}
+
+		let p_query = ''; if (req.body.trim != 'null') { p_query = ' AND '; }
+		if (req.body.country != 'null') {
+			p_query = p_query.concat(`f.s_key LIKE '%|${req.body.country}`);
+			if (req.body.city != 'null') { p_query = p_query.concat(`;${req.body.city}`); }
+			query = query.concat(p_query + '\'');
+		} else {
+			query = query.concat(`AND (f.s_key LIKE '%|Portugal' OR f.s_key LIKE '%|Espanha')`);
+		}
+	}
+	console.log(query)
+
+	sql.query(connectionString, query, (err, result) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		const Data = result.map((row) => ({
+			name: row.s_key,
+			value: row.net_total,
+		}));
+			console.log(Data);
+		res.render("newgraph5", { graphData: Data });
+	});
+};
+
+GraphController.getGraphsdiagram6 = function (req, res) {
+	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
+
+	let query = '';
+	if (req.body.country == 'null' && req.body.trim == 'null') {
+		query = 'SELECT f.s_key, f.net_total FROM facts AS f, dim_places AS p WHERE f.s_key = p.s_key';
+	} else {
+		query = 'SELECT * FROM facts AS f WHERE ';
+
+		let t_query = '';
+		if (req.body.trim != 'null') {
+			t_query = `f.s_key LIKE '2023;${req.body.trim}`;
+			if (req.body.month != 'null') { t_query = t_query.concat(";" + req.body.month); }
+			query = query.concat(t_query + '|%\' ');
+		}
+
+		let p_query = ''; if (req.body.trim != 'null') { p_query = ' AND '; }
+		if (req.body.country != 'null') {
+			p_query = p_query.concat(`f.s_key LIKE '%|${req.body.country}`);
+			if (req.body.city != 'null') { p_query = p_query.concat(`;${req.body.city}`); }
+			query = query.concat(p_query + '\'');
+		} else {
+			query = query.concat(`AND (f.s_key LIKE '%|Portugal' OR f.s_key LIKE '%|Espanha')`);
+		}
+	}
 
 	sql.query(connectionString, query, (err, result) => {
 		if (err) {
@@ -420,30 +257,6 @@ GraphController.getGraphsdiagram1Time = function(req, res) {
 
 		res.render("newgraph6", { graphData: Data });
 	});
-}
-
-GraphController.getGraphsdiagram5Time = function(req, res) {
-
-	const connectionString = `server=.;Database=${req.body.company}_Warehouse;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};`;
-
-	let query = "SELECT * FROM facts AS f WHERE f.s_key LIKE '%SUPPLIER' AND f.s_key LIKE '2023;" + req.body.trimester;
-	if (req.body.formMonth != 'null') { query = query.concat(";" + req.body.formMonth); }
-	query = query.concat("|%'");
-
-	sql.query(connectionString, query, (err, result) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		const Data = result.map((row) => ({
-			name: row.s_key,
-			value: row.net_total,
-		}));
-
-		console.log(Data);
-
-		res.render("newgraph5", { graphData: Data });
-	});
-}
+};
 
 module.exports = GraphController;
--- routes/users.js
+++ routes/users.js
@@ -26,16 +26,10 @@ router.get('/otheryear2020', function(req, res, next) {
 
 //router para paginas da diretory 5
 router.get('/directory5/ElectroTech', GraphController.getGraphsdiagram5);
-
 router.post('/directory1', GraphController.getGraphsdiagram1);
-
 router.post('/directory2', GraphController.getGraphsdiagram2);
-
 router.post('/directory3', GraphController.getGraphsdiagram3);
-
 router.post('/directory4', GraphController.getGraphsdiagram4);
-
-//router.post('/directory5', GraphController.getGraphsdiagram5);
 router.post('/directory5',GraphController.getGraphsdiagram5);
 router.post('/directory6', GraphController.getGraphsdiagram6);
 
